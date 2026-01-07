8b5d15779f82c03eab3906567199d1a63f203dd0
qrtourguidehistory-cpu <qrtourguidehistory@gmail.com>
2026-01-07 19:27:49 -0400

    arreglo appflow

--- dist/assets/index-R2e5z7Ai.js
+++
@@ -1,2 +0,0 @@
-const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-y2oLLOtm.js","./index-YNrFhYI6.js","./index-BVqcWWkG.css"])))=>i.map(i=>d[i]);
-import{au as e,al as o}from"./index-YNrFhYI6.js";const i=e("Browser",{web:()=>o(()=>import("./web-y2oLLOtm.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.BrowserWeb)});export{i as Browser};
