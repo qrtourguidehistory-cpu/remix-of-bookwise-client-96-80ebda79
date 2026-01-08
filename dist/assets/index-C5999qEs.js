d757486ea5adf347e6fa27b84ba1398f6f7b4677
qrtourguidehistory-cpu <qrtourguidehistory@gmail.com>
2026-01-07 22:34:40 -0400

    login google account funcional

---
+++ dist/assets/index-C5999qEs.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["./web-C_ihI500.js","./index-CqvD_sCs.js","./index-BVqcWWkG.css"])))=>i.map(i=>d[i]);
+import{au as e,_ as o}from"./index-CqvD_sCs.js";const _=e("Browser",{web:()=>o(()=>import("./web-C_ihI500.js"),__vite__mapDeps([0,1,2]),import.meta.url).then(r=>new r.BrowserWeb)});export{_ as Browser};
